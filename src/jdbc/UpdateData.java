4d3f81a22e13b1f43e4730490a9f99d93cc1c619
aklema094 <aklemasultana094@gmail.com>
2025-01-02 10:51:30 +0600

    update data in database

--- src/jdbc/UpdateData.java
+++ src/jdbc/UpdateData.java
@@ -1,4 +1,3 @@
-
 package jdbc;
 
 import java.sql.Connection;
@@ -7,7 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class DeleteData {
+public class UpdateData {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String url = "jdbc:mysql://localhost:3306/hospital";
@@ -17,30 +17,31 @@ public class DeleteData {
         System.out.print("Enter your password : ");
         String pass = sc.next();
         sc.nextLine();
-        
-        try{
+
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-        }catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
-            
+
         }
-        
-        try{
-            Connection con = DriverManager.getConnection(url,name,pass);
+
+        try {
+            Connection con = DriverManager.getConnection(url, name, pass);
             Statement st = con.createStatement();
-            int affectedRows = st.executeUpdate("DELETE FROM patients WHERE id = 5;");
-            if(affectedRows>0){
-                System.out.println("Patient deleted successfully");
-            }else{
-                System.out.println("Failed to delete Patient");
+            int affectedRows = st.executeUpdate("UPDATE patients SET name = 'Tahsin',age = 6 WHERE id = 4;");
+            if (affectedRows > 0) {
+                System.out.println("Updated patient successfully");
+            } else {
+                System.out.println("Failed to update Patient");
             }
-            
+
             st.close();
             con.close();
             System.out.println("Connection closed successfully");
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+
     }
 }
